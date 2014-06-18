5d3bbed96c7f984d0b0d897bb9ee12ba3da6bc00
hung-phan <colorvisavn@gmail.com>
2014-06-19 00:34:09 +0700

    working version for requirejs config

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module App
   class Application < Rails::Application
+    config.autoload_paths += %W(#{config.root}/lib)
     #config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
---
+++ lib/jasmine_rails/spec_helper.rb
@@ -0,0 +1,6 @@
+module JasmineRails
+  module SpecHelper
+    # Gives us access to the require_js_include_tag helper
+    include RequirejsHelper
+  end
+end
