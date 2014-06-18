7c332ec564616aa485faa571dd18330baa3e7eda
hung-phan <colorvisavn@gmail.com>
2014-06-18 14:23:19 +0700

    working on jasmine

--- spec/javascripts/support/jasmine_config.rb
+++ spec/javascripts/support/jasmine_config.rb
@@ -16,8 +16,10 @@ module Jasmine
   class RunAdapter
     def run(focused_suite = nil)
       jasmine_files = @jasmine_files
+      p jasmine_files
       css_files = @jasmine_stylesheets + (@config.css_files || [])
       js_files = @config.js_files(focused_suite)
+      p js_files
       body = ERB.new(File.read(File.join(File.dirname(__FILE__), "run.html.erb"))).result(binding)
       [
         200,
